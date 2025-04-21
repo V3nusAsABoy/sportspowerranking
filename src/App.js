8e4d2b4dbf44e3f4209d895bdbce0853a9d9b4aa
Andrew Abdulaziz <andrewalloniealt@gmail.com>
2025-04-21 13:05:43 -0400

    imported hawks and celtics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './main.css';
+import data from "./nba.json";
+
+document.getElementById("option1").setAttribute("src", data[0].logo);
+document.getElementById("option2").setAttribute("src", data[1].logo);
+document.getElementById("option1txt").innerHTML = data[0].team;
+document.getElementById("option2txt").innerHTML = data[1].team;
 
 function App() {
   return (
     <>
-      <div id = "option1"><img /></div>
-      <div id = "option2"><img /></div>
+      <div><img id = "option1"></img><p id = "option1txt"></p></div>
+      <div><img id = "option2"></img><p id = "option2txt"></p></div>
     </>
   )
 }
