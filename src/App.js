fa819ae1a29b729f40ce7fa3ecf3165af740bfbf
Andrew Abdulaziz <andrewalloniealt@gmail.com>
2025-04-21 12:43:04 -0400

    created nba.json

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './main.css';
 function App() {
   return (
     <>
-      <div id = "option1"></div>
-      <div id = "option2"></div>
+      <div id = "option1"><img /></div>
+      <div id = "option2"><img /></div>
     </>
   )
 }
