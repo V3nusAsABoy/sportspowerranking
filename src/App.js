fa0aa83e1287c3422987d54acc1b766b92ee68e2
Andrew Abdulaziz <andrewalloniealt@gmail.com>
2025-04-22 11:55:33 -0400

    created options div and added styling

--- src/App.js
+++ src/App.js
@@ -10,8 +10,10 @@ document.getElementById("option2txt").innerHTML = data[1].team;
 function App() {
   return (
     <>
+    <div id = "options">
       <div class = "option"><img id = "option1"></img><p id = "option1txt"></p></div>
       <div class = "option"><img id = "option2"></img><p id = "option2txt"></p></div>
+    </div>
     </>
   )
 }
