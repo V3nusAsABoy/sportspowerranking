da6f7c274bf40dd75eae9f0127c00706ac33fa85
Andrew Abdulaziz <andrewalloniealt@gmail.com>
2025-07-02 12:58:34 -0400

    changed results screen

--- src/Results.js
+++ src/Results.js
@@ -1,20 +1,37 @@
+import React, { useState} from 'react';
+
 const Results = ({teams}) => {
+
+    const [copied, setCopied] = useState(false);
+
+    function copy(){
+      
+      const list = teams.map((t, index) => `${index + 1}: ${t.name}\n`);
+
+      navigator.clipboard.writeText(list.join(""));
+
+      setCopied(true);
+    }
+
+
     return (
         <>
-        <div>
+        <div id = "results">
         <h1>Final Ranking</h1>
         <ol>
         {teams.map((t, index) => {
           return (
             <>
-              <li>{` ${teams[index].name}`}</li>
-              <img src={`${teams[index].logo}`} />
-              <hr />
+              <li>{` ${teams[index].name}`}<img src={`${teams[index].logo}`} /></li>
             </>
           )})
         }
         <br />
         </ol>
+        <div id ="copy">
+          <button onClick={copy}>Copy</button>
+          <p style = {{ visibility: copied ? 'visible' : 'hidden'}}>Copied to clipboard</p>
+        </div>
         </div>
         <br />
       </>
